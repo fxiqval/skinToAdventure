a43e4214346d03f192fa1475b3b4eed76f7a0c05
fxiq <fxiqval@gmail.com>
2023-05-27 18:09:05 +0200

    add java

--- index.js
+++ index.js
@@ -53,8 +53,13 @@ function parseToTellraw(){
 
 }
 
-function trToJava(){
-
+function trToJava(tr){
+    let final = [];
+    tr.forEach(element => {
+        if (typeof element === 'string') return final.push('Component.text(\'\\n\')'   )
+        final.push(`Component.text("${element.text}").color(TextColor.color(0x${element.color.slice(1)}))`);
+    });
+    return final.map((x, i) => i !== 0 ? x + ')' : x).join('.append(');
 }
 function trToKotlin(tr){
     let final = [];
